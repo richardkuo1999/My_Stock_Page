richardkuo1999/value_investment

[feat] Save the syslog

Database/Anue.py
@@ -1,16 +1,14 @@
 import os
 import sys
-import logging
 import aiohttp
 import asyncio
 from datetime import datetime
 from urllib.parse import unquote
 
 sys.path.append(os.path.dirname(__file__) + "/..")
-from utils.utils import fetch_webpage
+from utils.utils import logger_create, fetch_webpage
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logger_create(__name__)
 
 GOOGLE_SEARCH_BASE_URL = "https://www.google.com/search?q={}&num={}"
 YAHOO_SEARCH_BASE_URL = "https://tw.search.yahoo.com/search?p={}&fr=yfp-search-sb"

Database/Goodinfo.py
@@ -2,13 +2,11 @@ import os
 import sys
 import aiohttp
 import asyncio
-import logging
 
 sys.path.append(os.path.dirname(__file__) + "/..")
-from utils.utils import fetch_webpage
+from utils.utils import logger_create, fetch_webpage
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+logger = logger_create(__name__)
 
 GOODINFO_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",

Database/YahooFinance.py
@@ -2,11 +2,14 @@ import os
 import sys
 import aiohttp
 import asyncio
-import logging
 import yfinance as yf
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+sys.path.append(os.path.dirname(__file__) + "/..")
+
+from utils.utils import logger_create
+
+logger = logger_create(__name__)
+
 
 class YahooFinance:
     SUPPORTED_MARKETS = ["TW", "TWO"]

calculator/stock_select.py
@@ -1,22 +1,19 @@
 import re
 import os
 import sys
-import logging
 import aiohttp
 import asyncio
 from natsort import natsorted
 
 sys.path.append(os.path.dirname(__file__) + "/..")
 
-from utils.utils import fetch_webpage, is_ordinary_stock
+from utils.utils import logger_create, fetch_webpage, is_ordinary_stock
+
+logger = logger_create(__name__)
 
 ETF_BASE_URL = "https://www.moneydj.com/ETF/X/Basic/Basic0007B.xdjhtm?etfid={}.TW"
 INVESTOR_URL = "https://histock.tw/stock/three.aspx?s={}"
 INVESTOR_TYPES = {"foreign": "a", "investment_trust": "b", "dealers": "c"}
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 
 async def fetch_etf_constituents(session, etf_id: str) -> list[str]:
     try:

utils/utils.py
@@ -13,8 +13,6 @@ from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_t
 # from googleapiclient.http import MediaFileUpload
 # from googleapiclient.errors import HttpError
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 DEFAULT_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
@@ -22,6 +20,22 @@ DEFAULT_HEADERS = {
     "Accept-Language": "zh-TW,zh;q=0.9,en;q=0.8",
 }
 
+
+def logger_create(file_name):
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s:%(funcName)s:%(lineno)d - %(message)s",
+    )
+    logger = logging.getLogger(file_name)
+    file_handler = logging.FileHandler("syslog.log")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(
+        logging.Formatter("%(asctime)s - %(levelname)s - %(name)s:%(funcName)s:%(lineno)d - %(message)s",)
+    )
+    logger.addHandler(file_handler)
+    return logger
+
+logger = logger_create(__name__)
 
 @retry(
     stop=stop_after_attempt(3),

richardkuo1999/My_Stock_Page

value_investment/value_investment/Database/Anue.py
@@ -1,16 +1,13 @@
 import os
 import sys
-import logging
 import aiohttp
 import asyncio
 from datetime import datetime
 from urllib.parse import unquote
 
 sys.path.append(os.path.dirname(__file__) + "/..")
-from utils.utils import fetch_webpage
+from utils.utils import logger, fetch_webpage
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 GOOGLE_SEARCH_BASE_URL = "https://www.google.com/search?q={}&num={}"
 YAHOO_SEARCH_BASE_URL = "https://tw.search.yahoo.com/search?p={}&fr=yfp-search-sb"

value_investment/value_investment/Database/Goodinfo.py
@@ -2,13 +2,10 @@ import os
 import sys
 import aiohttp
 import asyncio
-import logging
 
 sys.path.append(os.path.dirname(__file__) + "/..")
-from utils.utils import fetch_webpage
+from utils.utils import logger, fetch_webpage
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 GOODINFO_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",

value_investment/value_investment/Database/YahooFinance.py
@@ -2,11 +2,12 @@ import os
 import sys
 import aiohttp
 import asyncio
-import logging
 import yfinance as yf
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+sys.path.append(os.path.dirname(__file__) + "/..")
+
+from utils.utils import logger
+
 
 class YahooFinance:
     SUPPORTED_MARKETS = ["TW", "TWO"]

value_investment/value_investment/calculator/stock_select.py
@@ -1,22 +1,18 @@
 import re
 import os
 import sys
-import logging
 import aiohttp
 import asyncio
 from natsort import natsorted
 
 sys.path.append(os.path.dirname(__file__) + "/..")
 
-from utils.utils import fetch_webpage, is_ordinary_stock
+from utils.utils import logger, fetch_webpage, is_ordinary_stock
+
 
 ETF_BASE_URL = "https://www.moneydj.com/ETF/X/Basic/Basic0007B.xdjhtm?etfid={}.TW"
 INVESTOR_URL = "https://histock.tw/stock/three.aspx?s={}"
 INVESTOR_TYPES = {"foreign": "a", "investment_trust": "b", "dealers": "c"}
-
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
 
 async def fetch_etf_constituents(session, etf_id: str) -> list[str]:
     try:

value_investment/value_investment/utils/utils.py
@@ -13,8 +13,6 @@ from tenacity import retry, stop_after_attempt, wait_fixed, retry_if_exception_t
 # from googleapiclient.http import MediaFileUpload
 # from googleapiclient.errors import HttpError
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
 
 DEFAULT_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
@@ -22,6 +20,22 @@ DEFAULT_HEADERS = {
     "Accept-Language": "zh-TW,zh;q=0.9,en;q=0.8",
 }
 
+
+def logger_create(file_name):
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(levelname)s - %(name)s:%(funcName)s:%(lineno)d - %(message)s",
+    )
+    logger = logging.getLogger(__name__)
+    file_handler = logging.FileHandler("syslog.log")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(
+        logging.Formatter("%(asctime)s - %(levelname)s - %(filename)s:%(funcName)s:%(lineno)d - %(message)s",)
+    )
+    logger.addHandler(file_handler)
+    return logger
+
+logger = logger_create(__name__)
 
 @retry(
     stop=stop_after_attempt(3),